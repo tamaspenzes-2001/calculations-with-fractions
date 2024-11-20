7580ac9978d18a25df131d3f142f03754a0bbf3c
tamaspenzes-2001 <penzestamas@proton.me>
2024-11-20 17:21:25 +0100

    implement division

--- src/calculationsWithFractions/Calculator.java
+++ src/calculationsWithFractions/Calculator.java
@@ -29,10 +29,19 @@ public class Calculator {
 		return new Fraction(numerator, fraction1.getDenominator());
 	}
 	
-	public Fraction multiply() {
-		int numerator = fraction1.getNumerator() * fraction2.getNumerator();
-		int denominator = fraction1.getDenominator() * fraction2.getDenominator();
+	public Fraction multiply(Fraction... reciprocal) {
+		Fraction multiplyer = fraction2;
+		if (reciprocal.length == 1) {
+			multiplyer = reciprocal[0];
+		}
+		int numerator = fraction1.getNumerator() * multiplyer.getNumerator();
+		int denominator = fraction1.getDenominator() * multiplyer.getDenominator();
 		return new Fraction(numerator, denominator);
+	}
+	
+	public Fraction divide() {
+		Fraction reciprocalOfFraction2 = new Fraction(fraction2.getDenominator(), fraction2.getNumerator());
+		return multiply(reciprocalOfFraction2);
 	}
 	
 	private void bringToLowestCommonDenominator() {
